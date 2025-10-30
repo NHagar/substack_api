65e2d0a9235559ba2273a436455bb3e11cc4c929
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-10-30 15:18:32 -0500

    dynamic version num from release tags

--- substack_api/__init__.py
+++ substack_api/__init__.py
@@ -1,8 +1,16 @@
+from importlib import metadata as _metadata
+
 from .auth import SubstackAuth
 from .category import Category, list_all_categories
 from .newsletter import Newsletter
 from .post import Post
 from .user import User, resolve_handle_redirect
+
+try:
+    # Use distribution metadata so tag-based builds report the correct version.
+    __version__ = _metadata.version("substack-api")
+except _metadata.PackageNotFoundError:  # pragma: no cover - occurs for editable installs
+    __version__ = "0.0.0"
 
 __all__ = [
     "User",
@@ -12,4 +20,5 @@ __all__ = [
     "SubstackAuth",
     "resolve_handle_redirect",
     "list_all_categories",
+    "__version__",
 ]
